29128458c61c6ae6219664d3e7e8247a9b457ae2
gabriellygomes16 <gabriellyrodriguesge@gmail.com>
2024-11-29 19:24:26 -0300

    Add files via upload

---
+++ assets/perguntas.js
@@ -0,0 +1,43 @@
+criaCartao(
+    'Biologia',
+    'Quais as características das Angiospermas?',
+    'A principal característica das angiospermas é a presença de flores e frutos que envolvem e protegem as sementes contendo o embrião. Por terem os órgãos reprodutores bem visíveis e produzirem sementes, as angiospermas, assim como as gimnospermas, são chamadas de fanerógamas.',
+)
+
+criaCartao(
+    'Filosofia',
+    'O que é o mito da caverna?',
+    ' o mito da caverna de Platão é uma alegoria complexa e multifacetada que ilustra a natureza da realidade, a busca pela verdade, a relação entre o mundo sensível e o mundo das ideias, a educação e a política. Ele continua sendo uma das histórias mais conhecidas e influentes na filosofia ocidental.',
+)
+
+criaCartao(
+    'Química',
+    'Qual é o produto da eletrólise da água?',
+    ' O produto da eletrólise da água é a decomposição da água em hidrogênio (H2) e oxigênio (O2)',
+
+)
+
+criaCartao(
+    'Filosofia',
+    'Qual á a teoria de Thomas Hobbes?',
+    ' Grande defensor do absolutismo, Hobbes defende essa forma de governo utilizando argumentos lógicos e estritamente racionais (excluindo quaisquer preceitos ou argumentos religiosos). Sua teoria baseia-se na ideia de que é necessário um Estado Soberano para controlar a todos e manter a paz civil..',
+)
+
+criaCartao(
+    'Biologia',
+    'Como ocorre o preocesso de hematose?',
+    ' A hematose acontece quando o oxigênio proveniente da respiração pulmonar chega aos alvéolos pulmonares. Nesse local, o oxigênio difunde-se para o sangue presente nos capilares à sua volta e o gás carbônico presente no sangue dos capilares difunde-se para o interior dos alvéolos (trocas gasosas).',
+)
+
+criaCartao(
+    'História',
+    'Por que Getúlio Vargas é considerado um líder populista?',
+    ' O presidente se comunicava de maneira simples e popular com as massas, o governo utilizava o rádio divulgando os avanços e ganhos dos trabalhadores. Assim, ficou conhecido como pai dos pobres',
+)
+
+
+
+
+
+
+
